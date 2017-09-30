c11537c628245217b953bf438095fa38063eda3b
Shantell Miller <shantellmiller28@gmail.com>
2017-09-30 19:23:15 +0000

    If statement

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,8 +3,14 @@ class TodoController <ApplicationController
     end
     
     def show
-        @todo_description = "Clean my room"
-        @todo_pomodoro_estimate = 4
+        todo_id='2'
+        
+        if todo_id=='1'
+            @todo_description="Clean my room"
+            @todo_pomodoro_estimate =4
+        elsif todo_id=='2'
+            @todo_description="Finish my homework"
+            @todo_pomodoro_estimate= 2
+        end
     end
-    
 end
